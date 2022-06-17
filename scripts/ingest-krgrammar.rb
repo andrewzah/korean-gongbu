30394768a612d7fae9f9bfa28030153313896c5e
Andrew Zah <zah@andrewzah.com>
2022-06-17 12:43:05 +0900

    use ASDF, update db init script

--- scripts/ingest-krgrammar.rb
+++ scripts/ingest-krgrammar.rb
@@ -16,17 +16,25 @@ require 'pg'
 require 'json'
 
 begin
-  con = PG.connect({ :host => '0.0.0.0', :port => 5444, :dbname => 'gongbu', :user => 'gongbu', :password => 'gongbu' })
+  con = PG.connect({ :host => '0.0.0.0', :port => 5444, :dbname => 'gongbu_dev', :user => 'gongbu', :password => 'gongbu' })
 
-  con.exec "DROP TABLE IF EXISTS grammars"
-  con.exec "CREATE TABLE grammars (id SERIAL, grammar_name TEXT, translation_en TEXT)"
+  ### this is managed with rails now, do db:migrate ###
+  #
+  #con.exec "DROP TABLE IF EXISTS grammars"
+  #con.exec "CREATE TABLE grammars (id SERIAL,
+  #  grammar_name TEXT,
+  #  translation_en TEXT,
+  #  category_en TEXT,
+  #  additional_info TEXT)"
 
   con.exec "DELETE FROM grammars where 1=1"
-  con.prepare('stm1', 'INSERT INTO grammars (grammar_name, translation_en) VALUES ($1, $2)')
+  con.prepare('stm1', 'INSERT INTO grammars (name, translation_en, category_en, additional_info, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)')
+
+  timestamp = Time.now
 
   grammars = JSON.parse(File.read('../data/krgrammar.json'))
   grammars.each do |g|
-    con.exec_prepared('stm1', [g['name'], g['translation']])
+    con.exec_prepared('stm1', [g['name'], g['translation'], g['category_en'], g['additional_info'], timestamp, timestamp])
   end
 
   rs = con.exec('SELECT count(*) from grammars')
