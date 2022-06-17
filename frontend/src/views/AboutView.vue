78e98f0d6797e35577b10c77ced12b43f9a4816c
Andrew Zah <zah@andrewzah.com>
2022-06-17 12:41:58 +0900

    RIP svelte, welcome vue.js

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,49 @@
+<script>
+  import { Search } from 'sonic-channel';
+
+  let sonicChannelSearch = new Search({
+    host : "0.0.0.0",
+    port : 1492
+  }).connect({
+    connected : function() {
+      // Connected handler
+      console.info("Sonic Channel succeeded to connect to host (search).");
+    },
+
+    disconnected : function() {
+      // Disconnected handler
+      console.error("Sonic Channel is now disconnected (search).");
+    },
+
+    timeout : function() {
+      // Timeout handler
+      console.error("Sonic Channel connection timed out (search).");
+    },
+
+    retrying : function() {
+      // Retry handler
+      console.error("Trying to reconnect to Sonic Channel (search)...");
+    },
+
+    error : function(error) {
+      // Failure handler
+      console.error("Sonic Channel failed to connect to host (search).", error);
+    }
+  });
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
