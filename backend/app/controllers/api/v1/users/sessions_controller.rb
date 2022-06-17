ff6123c608465bd20781d3b40588fa07b328af77
Andrew Zah <zah@andrewzah.com>
2022-06-17 21:24:26 +0900

    WIP: devise jwt

---
+++ backend/app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,65 @@
+module Api
+  module V1
+    module Users
+      class SessionsController < Devise::SessionsController
+        respond_to :json
+
+        private
+
+        def respond_with(resource, _opts = {})
+          puts "resource: "
+          p resource
+          render json: { message: 'You are logged in.' }, status: :ok
+        end
+
+        def respond_to_on_destroy
+          log_out_success && return if current_user
+
+          log_out_failure
+        end
+
+        def log_out_success
+          render json: { message: "You are logged out." }, status: :ok
+        end
+
+        def log_out_failure
+          render json: { message: "Hmm nothing happened."}, status: :unauthorized
+        end
+
+        # before_action only: [:destroy] do 
+        #   authenticate_cookie
+        # end
+
+        # def destroy
+        #   user = current_user
+        #   if user  
+        #     cookies.delete(:jwt)
+        #     render json: {status: 'OK', code: 200}
+        #   else
+        #     render json: {status: 'session not found', code: 404}
+        #   end
+        # end
+
+        # def create
+        #   email = params["email"]
+        #   password = params["password"]
+
+        #   if email && password
+        #     login_hash = User.handle_login(email, password)
+        #     if login_hash
+        #       cookies.signed[:jwt] = {value:  login_hash[:token], httponly: true}
+        #       render json: { 
+        #         user_id: login_hash[:user_id],
+        #         name: login_hash[:name],
+        #       }
+        #     else
+        #       render json: {status: 'incorrect email or password', code: 422}  
+        #     end
+        #   else
+        #     render json: {status: 'specify email address and password', code: 422}
+        #   end
+        # end
+      end
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :v1 do
+      devise_for :users, 
+        path_names: {
+          sign_in: 'login',
+          sign_out: 'logout',
+          registration: 'signup'
+        },
+        controllers: {
+          sessions: 'api/v1/users/sessions',
+          registrations: 'api/v1/users/registrations'
+        }
+
       resources :grammar
       post "/grammar/search", to: "grammar#search"
     end
